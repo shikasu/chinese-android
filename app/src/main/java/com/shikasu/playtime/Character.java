076b002422da24542a1f05fbccbbcf4589eddb8a
Julien D <julien@duraj.fr>
2016-02-27 12:53:56 +0000

    Define a Character and a Phrase

---
+++ app/src/main/java/com/shikasu/playtime/Character.java
@@ -0,0 +1,10 @@
+package com.shikasu.playtime;
+
+/**
+ * Created by jul on 2/27/16.
+ */
+public class Character {
+    String chinese;
+    String english;
+    String pinyin;
+}
---
+++ app/src/main/java/com/shikasu/playtime/Phrase.java
@@ -0,0 +1,35 @@
+package com.shikasu.playtime;
+
+import java.util.List;
+
+/**
+ * Created by jul on 2/27/16.
+ */
+public class Phrase {
+    List<Character> phrase;
+    String pinyin = null;
+    String english = null;
+
+    String getChinese() {
+        String constructedChinese = "";
+        for (int i = 0; i < phrase.size(); i++) {
+            constructedChinese += phrase.get(i).chinese;
+        }
+        return constructedChinese;
+    }
+
+    String getPinyin() {
+        if (this.pinyin == null) {
+            String constructedPinyin = "";
+            for (int i = 0; i < phrase.size(); i++) {
+                constructedPinyin += phrase.get(i).pinyin + " ";
+            }
+            return constructedPinyin.substring(0, constructedPinyin.length() - 1);
+        }
+        return this.pinyin;
+    }
+
+    String getEnglish() {
+        return this.english;
+    }
+}
