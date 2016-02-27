32b96e8daf1d2a50d3ad658c0934f602e32069f5
Julien D <julien@duraj.fr>
2016-02-27 17:07:07 +0000

    (gradle) Updated libs + added tinypinyin which we will use as a fallback for Sentences without pinyin

--- app/src/main/java/com/shikasu/playtime/Character.java
+++ app/src/main/java/com/shikasu/playtime/Character.java
@@ -4,7 +4,16 @@ package com.shikasu.playtime;
  * Created by jul on 2/27/16.
  */
 public class Character {
-    String chinese;
-    String english;
-    String pinyin;
+    String mChinese;
+    String mEnglish;
+    String mPinyin;
+
+    Character(String chinese) {
+        new Character(chinese, null);
+    }
+
+    Character(String chinese, String english) {
+        this.mChinese = chinese;
+        this.mEnglish = english;
+    }
 }
